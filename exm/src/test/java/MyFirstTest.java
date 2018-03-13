aae4e0e618bc99c821faf7a2dd7ed19a8fdb9ace
artemiylednev <37328680+artemiylednev@users.noreply.github.com>
2018-03-13 19:40:23 +0300

    Тест поиска в гугле

---
+++ exm/src/test/java/MyFirstTest.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+
+public class MyFirstTest {
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start() {
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver,10);
+    }
+
+    @Test
+    public void myFirstTest()  {
+        driver.get("http://www.google.ru/");
+        driver.findElement(By.id("lst-ib")).sendKeys("webdriver\n");
+        driver.findElement(By.id("lst-ib")).sendKeys(Keys.ENTER);
+        wait.until(titleIs("webdriver - Поиск в Google"));
+    }
+
+    @After
+    public void stop() {
+        driver.quit();
+        driver = null;
+    }
+}
